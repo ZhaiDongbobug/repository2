fef45ecddb36b526fa92ddbb95613ec548d0bc9b
ZhaiDongboBug <18747867831@163.com>
2019-06-18 11:53:33 +0800

    变量的添加，和窗口的设定
    第一次完善

--- SwingPlane Demo1/src/game/game/MyGame.java
+++ SwingPlane Demo1/src/game/game/MyGame.java
@@ -1,5 +1,32 @@
 package game.game;
 
+import java.awt.image.BufferedImage;
+
+import game.util.Rect;
+
 public class MyGame {
 
+	public static final int FLUSHTIME = 100;//游戏的每帧刷新间隔时间，匀速刷新
+	//显示游戏屏幕的宽高
+	public static final int ScreenWidth = 480;
+	public static final int ScreenHeight = 852;
+	public static final Rect SCREEN_RECT = new Rect(0,0,
+			ScreenWidth,ScreenHeight);
+	public static BufferedImage[] ImagePool;
+	public static final int IMG_ENMEY1 = 0;
+	public static final int IMG_BACKGROUND = 1;
+	public static final int IMG_BEE = 2;
+	public static final int IMG_BULLET = 3;
+	public static final int IMG_GAMEOVER = 4;
+	public static final int IMG_HERO0 = 5;
+	public static final int IMG_HERO1 = 6;
+	public static final int IMG_PASUE = 7;
+	public static final int IMG_START = 8;
+	
+	int game_state = 0;//控制游戏状态的状态机
+	public static final int STATE_START = 0;
+	public static final int STATE_GAMING = STATE_START + 1;
+	public static final int STATE_PAUSE = STATE_GAMING + 1;
+	public static final int STATE_OVER = STATE_PAUSE + 1;
+	
 }
--- SwingPlane Demo1/src/game/util/Rect.java
+++ SwingPlane Demo1/src/game/util/Rect.java
@@ -1,5 +1,16 @@
 package game.util;
 
 public class Rect {
+	public int left,right,top,bottom;
+	public Rect(int x, int y, int w, int h) {
+		setPostion(x, y, w, h);
+	}
 
+	private void setPostion(int x, int y, int w, int h) {
+		// TODO Auto-generated method stub
+		left = x;
+		top = y;
+		right = x + w;
+		bottom = y + h;
+	}
 }
