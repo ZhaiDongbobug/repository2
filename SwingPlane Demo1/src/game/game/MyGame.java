2910b7a2df1d3b4318155d70fe20fa04c4580390
ZhaiDongboBug <18747867831@163.com>
2019-06-19 19:06:24 +0800

    初始化部分完善

--- SwingPlane Demo1/src/game/game/MyGame.java
+++ SwingPlane Demo1/src/game/game/MyGame.java
@@ -1,10 +1,18 @@
 package game.game;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
+import javax.swing.JPanel;
+
+import game.sprite.Bullet;
+import game.sprite.Enemy;
+import game.sprite.Hero;
 import game.util.Rect;
 
-public class MyGame {
+public class MyGame extends JPanel implements Runnable {
 
 	public static final int FLUSHTIME = 100;//游戏的每帧刷新间隔时间，匀速刷新
 	//显示游戏屏幕的宽高
@@ -28,5 +36,88 @@ public class MyGame {
 	public static final int STATE_GAMING = STATE_START + 1;
 	public static final int STATE_PAUSE = STATE_GAMING + 1;
 	public static final int STATE_OVER = STATE_PAUSE + 1;
+	
+	Hero hero;
+	ArrayList<Enemy> enemys;
+	int EnemySpeed = 1;
+	int EnemyCoolTime = 1;
+	int EnemyBirthTime = 1;
+	
+	ArrayList<Bullet> bullets;
+	int BulletSpeed = 1;
+	int BulletCoolTime = 1;
+	int BulletBirthTime = 1;
+	
+	public MyGame() {
+		initImage();
+		initAdapter();
+		initSprite();
+		
+		game_state = STATE_START;
+	}
+
+	private void initSprite() {
+		// TODO Auto-generated method stub
+		BufferedImage[] heroimgs = new BufferedImage[2];
+		heroimgs[0] = ImagePool[IMG_HERO0];
+		heroimgs[1] = ImagePool[IMG_HERO1];
+		int herox = 0;
+		int heroy = 0;
+		hero = new Hero(heroimgs, herox, heroy);
+		enemys = new ArrayList<Enemy>();
+		bullets = new ArrayList<Bullet>();
+	}
+
+	private void initAdapter() {
+		// TODO Auto-generated method stub
+		MouseAdapter ma = new MouseAdapter() {
+			public void mouseMoved(MouseEvent event) {
+				if(game_state == STATE_GAMING) {
+					hero.setPostion(event.getX(), event.getY());
+					return;//为什么要return
+				}
+				super.mouseMoved(event);//查一下日志，了解其逻辑
+			}
+			public void mouseEntered(MouseEvent arg0) {//了解参数的含义
+				if(game_state == STATE_PAUSE) {
+					game_state = STATE_GAMING;
+					return;
+				}
+				super.mouseEntered(arg0);
+			}
+			public void mouseExited(MouseEvent arg0) {
+				if(game_state == STATE_GAMING) {
+					game_state = STATE_PAUSE;
+					return;
+				}
+				super.mouseExited(arg0);
+			}
+			public void mouseClicked(MouseEvent e) {
+				if(game_state == STATE_PAUSE) {
+					game_state = STATE_GAMING;
+					return;
+				}else if(game_state == STATE_OVER) {
+					game_state = STATE_START;
+					return;
+				}
+				super.mouseClicked(e);
+			}
+		};
+		this.addMouseListener(ma);
+		this.addMouseMotionListener(ma);
+		
+	}
+
+	private void initImage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	
 }
--- SwingPlane Demo1/src/game/sprite/Hero.java
+++ SwingPlane Demo1/src/game/sprite/Hero.java
@@ -1,5 +1,16 @@
 package game.sprite;
 
+import java.awt.image.BufferedImage;
+
 public class Hero {
+
+	public Hero(BufferedImage[] heroimgs, int herox, int heroy) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setPostion(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
